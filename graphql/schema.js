8de36582369329a7a3f43a80186e5fe3e23dc131
Wojciech Bozek <bozek.wojciech@gmail.com>
2017-11-22 22:06:16 +0100

    Change schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -12,24 +12,20 @@ const OrganizationTC = composeWithMongoose(OrganizationModel, customizationOptio
 // via graphql-compose it will be much much easier, with less typing
 GQC.rootQuery().addFields({
   organizationById: OrganizationTC.getResolver('findById'),
-  userById: UserTC.getResolver('findById'),
+  organizationMany: OrganizationTC.getResolver('findMany'),
   userByIds: UserTC.getResolver('findByIds'),
   userOne: UserTC.getResolver('findOne'),
   userMany: UserTC.getResolver('findMany'),
-  userCount: UserTC.getResolver('count'),
   userConnection: UserTC.getResolver('connection'),
-  userPagination: UserTC.getResolver('pagination'),
 });
 
 GQC.rootMutation().addFields({
+  organizationCreate: OrganizationTC.getResolver('createOne'),
+  organizationUpdateById: OrganizationTC.getResolver('updateById'),
+  organizationRemoveById: OrganizationTC.getResolver('removeById'),
   userCreate: UserTC.getResolver('createOne'),
   userUpdateById: UserTC.getResolver('updateById'),
-  userUpdateOne: UserTC.getResolver('updateOne'),
-  userUpdateMany: UserTC.getResolver('updateMany'),
   userRemoveById: UserTC.getResolver('removeById'),
-  userRemoveOne: UserTC.getResolver('removeOne'),
-  userRemoveMany: UserTC.getResolver('removeMany'),
-  organizationCreate: OrganizationTC.getResolver('createOne'),
 });
 
 const graphqlSchema = GQC.buildSchema();
