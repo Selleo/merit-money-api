6cb6d2162848618421cfe16cafb2b8cf82c4e5e6
Mariusz Sznajder <sznajdermar@gmail.com>
2018-06-12 21:48:06 +0200

    add email uniqueness

--- db/migrate/20180609142034_create_users.rb
+++ db/migrate/20180609142034_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :email, unique: true
+      t.string :email, null: false
       t.string :name
       t.timestamps
     end
