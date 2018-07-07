110a4fd082493b3789ceca36b20848012b68ce35
Mariusz Sznajder <sznajdermar@gmail.com>
2018-07-07 11:25:40 +0200

    applies feedback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,7 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  rescue_from ActiveRecord::StatementInvalid, with: :render_invalid_statement_response
-
-  def render_unprocessable_entity_response(exception)
-    render json: exception.record.errors, status: :unprocessable_entity
-  end
 
   def render_not_found_response(exception)
     render json: { error: exception.message }, status: :not_found
-  end
-
-  def render_invalid_statement_response(exception)
-    render json: { error: exception.message }, status: :bad_request
   end
 end
